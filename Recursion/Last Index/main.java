87fef3b61c0d1983ede849eb66e49c11b62fe1e8
prashant208 <vb05446@gmail.com>
2024-03-28 18:12:29 +0530

    Last Index

---
+++ Recursion/Last Index/main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static int lastIndex(int []arr, int idx, int x){
+        if(idx == arr.length){
+            return -1;
+        }
+        int li = lastIndex(arr, idx+1, x);
+        if(li != -1){
+            return li;
+        }else{
+            if(arr[i] == x){
+                return idx;
+            }else{
+                return -1;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int []arr = new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        int x = scn.nextInt();
+        int ans = lastIndex(arr, 0, x);
+        System.out.println(ans);
+    }
+}
